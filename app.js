627687acf46a0e8937efe3e3ae0a0a98593e6ef6
Enric <enricorillosa@hotmail.es>
2022-10-09 22:04:38 +0200

    updated 2
    Fixed many data problems and CSS details on fixes. txt

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const cropInfo = [
     location: "Frosted Heights",
     seedPrice: 150,
     time: "2h 15min",
+    bonusTime: "2h 2min",
     sellPrice: 133,
     waters: 2,
     yield: 3,
@@ -27,6 +28,7 @@ const cropInfo = [
     location: "Forest of Valor",
     seedPrice: 12,
     time: "15min",
+    bonusTime: "13.5min",
     sellPrice: 33,
     yield: 1,
     waters: 1,
@@ -37,6 +39,7 @@ const cropInfo = [
     location: "Peaceful Meadow",
     seedPrice: 10,
     time: "15min",
+    bonusTime: "13.5min",
     sellPrice: 44,
     yield: 1,
     waters: 1,
@@ -47,6 +50,7 @@ const cropInfo = [
     location: "Sunlit Plateau",
     seedPrice: 20,
     time: "45 min",
+    bonusTime: "40.5min",
     sellPrice: 78,
     yield: 1,
     waters: 1,
@@ -57,6 +61,7 @@ const cropInfo = [
     location: "Frosted Heights",
     seedPrice: 40,
     time: "1h 15min",
+    bonusTime: "1h 8min",
     sellPrice: 159,
     yield: 1,
     waters: 1,
@@ -67,6 +72,7 @@ const cropInfo = [
     location: "Frosted Heights",
     seedPrice: 95,
     time: "3h",
+    bonusTime: "2h 42min",
     sellPrice: 308,
     yield: 1,
     waters: 2,
@@ -77,6 +83,7 @@ const cropInfo = [
     location: "Forgotten Lands",
     seedPrice: 120,
     time: "2h",
+    bonusTime: "1h 48min",
     sellPrice: 309,
     yield: 1,
     waters: 2,
@@ -87,6 +94,7 @@ const cropInfo = [
     location: "Peaceful Meadow",
     seedPrice: 3,
     time: "3min",
+    bonusTime: "2.7min",
     sellPrice: 8,
     yield: 1,
     waters: 1,
@@ -97,6 +105,7 @@ const cropInfo = [
     location: "Glade of Trust",
     seedPrice: 135,
     time: "2h",
+    bonusTime: "1h 48min",
     sellPrice: 114,
     yield: 3,
     waters: 1,
@@ -107,6 +116,7 @@ const cropInfo = [
     location: "Forest of Valor",
     seedPrice: 50,
     time: "1h 15min",
+    bonusTime: "1h 8min",
     sellPrice: 170,
     yield: 1,
     waters: 3,
@@ -117,6 +127,7 @@ const cropInfo = [
     location: "Forgotten Lands",
     seedPrice: 55,
     time: "35min",
+    bonusTime: "31.5min",
     sellPrice: 126,
     yield: 1,
     waters: 3,
@@ -127,6 +138,7 @@ const cropInfo = [
     location: "Forgotten Lands",
     seedPrice: 275,
     time: "4h",
+    bonusTime: "3h 36min",
     sellPrice: 664,
     yield: 1,
     waters: 2,
@@ -137,6 +149,7 @@ const cropInfo = [
     location: "Glade of Trust",
     seedPrice: 45,
     time: "1h",
+    bonusTime: "54min",
     sellPrice: 41,
     yield: 3,
     waters: 2,
@@ -146,86 +159,111 @@ const cropInfo = [
     name: "Tomato",
     location: "Dazzle Beach",
     seedPrice: 8,
-    time: 0.4,
-    sellPrice: 66,
+    time: "25min",
+    bonusTime: "22.5min",
+    sellPrice: 22,
+    yield: 3,
     waters: 2,
-    img: "https://dreamlightvalley.wikizet.com/english/images/9/9d/Tomato.png",
+    img: "https://dreamlightvalley.wikizet.com/english/images/3/3f/Tomato_Seed.png",
   },
   {
     name: "Zucchini",
     location: "Sunlit Plateau",
     seedPrice: 30,
-    time: 0.67,
-    sellPrice: 104,
+    time: "40min",
+    bonusTime: "36min",
+    sellPrice: 52,
+    yield:2,
     waters: 2,
-    img: "https://dreamlightvalley.wikizet.com/english/images/f/f6/Zucchini.png",
+    img: "https://dreamlightvalley.wikizet.com/english/images/5/5e/Zucchini_Seed.png",
   },
   {
     name: "Rice",
     location: "Glade of Trust",
     seedPrice: 35,
-    time: 0.83,
-    sellPrice: 122,
+    time: "50min",
+    bonusTime: "45min",
+    sellPrice: 61,
+    yield: 2,
     waters: 1,
-    img: "https://dreamlightvalley.wikizet.com/english/images/d/da/Rice.png",
+    img: "https://dreamlightvalley.wikizet.com/english/images/9/9b/Rice_Seed.png",
   },
   {
     name: "Wheat",
     location: "Peaceful Meadow",
     seedPrice: 1,
-    time: 0.017,
-    sellPrice: 4,
+    time: "1min",
+    bonusTime: "54s",
+    sellPrice: 2,
+    yield: 2,
     waters: 1,
-    img: "https://dreamlightvalley.wikizet.com/english/images/e/e2/Wheat.png",
+    img: "https://dreamlightvalley.wikizet.com/english/images/7/73/Wheat_Seed.png",
   },
   {
     name: "Canola",
     location: "Forest of Valor",
     seedPrice: 25,
-    time: 0.6,
+    time: "35min",
+    bonusTime: "31.5min",
     sellPrice: 109,
+    yield: 1,
     waters: 4,
-    img: "https://dreamlightvalley.wikizet.com/english/images/7/79/Canola.png",
+    img: "https://dreamlightvalley.wikizet.com/english/images/5/52/Canola_Seed.png",
   },
   {
     name: "Corn",
     location: "Dazzle Beach",
     seedPrice: 15,
-    time: 0.2,
-    sellPrice: 32,
+    time: "12min",
+    bonusTime: "10.8min",
+    sellPrice: 16,
+    yield: 2,
     waters: 1,
-    img: "https://dreamlightvalley.wikizet.com/english/images/f/f8/Corn.png",
+    img: "https://dreamlightvalley.wikizet.com/english/images/5/58/Corn_Seed.png",
   },
   {
     name: "Soya",
     location: "Sunlit Plateau",
     seedPrice: 60,
-    time: 1.5,
-    sellPrice: 207,
+    time: "1h 30min",
+    bonusTime: "1h 21min",
+    sellPrice: 69,
+    yield: 3,
     waters: 2,
-    img: "https://dreamlightvalley.wikizet.com/english/images/0/00/Soya.png",
+    img: "https://dreamlightvalley.wikizet.com/english/images/d/dd/Soya_Seed.png",
   },
   {
     name: "Sugarcane",
     location: "Dazzle Beach",
     seedPrice: 5,
-    time: 0.12,
+    time: "7min",
+    bonusTime: "6.3min",
     sellPrice: 19,
+    yield: 1,
     waters: 1,
-    img: "https://dreamlightvalley.wikizet.com/english/images/0/03/Sugarcane.png",
+    img: "https://dreamlightvalley.wikizet.com/english/images/d/dc/Sugarcane_Seed.png",
   },
   {
     name: "Cotton",
     location: "Sunlit Plateau",
-    seedPrice: 42,
-    time: 0.42,
-    sellPrice: 37,
+    seedPrice: 37,
+    time: "25min",
+    bonusTime: "22.5min",
+    sellPrice: 42,
+    yield: 1,
     waters: 4,
-    img: "https://dreamlightvalley.wikizet.com/english/images/thumb/c/c3/Cotton.png/103px-Cotton.png",
+    img: "https://dreamlightvalley.wikizet.com/english/images/9/97/Cotton_Seed.png",
   },
 ];
 
 // ----------------------------------------------------------------------------------------------------------
+
+//selectize js
+$(document).ready(function () {
+  $('select').selectize({
+      sortField: 'text'
+  });
+});
 
 //query selectors:
 //select
@@ -269,23 +307,20 @@ function calcularProfit() {
   resultadoFinal.style.display = "block";
   let selected = document.getElementById("cultivo");
   let inputNumber = document.getElementById("cantidad");
-//operations to calculate cost, net profi and show: where to plant, watering times.
+//operations to calculate cost, net profit and show: where to plant, watering times.
   for (const cropObject2 of cropInfo) {
     let coste = cropObject2.seedPrice * inputNumber.value;
-    let beneficio = ((cropObject2.sellPrice * inputNumber.value) - coste);
+    let beneficio = ((cropObject2.sellPrice * inputNumber.value * cropObject2.yield) - coste);
 //print on final result div
     if (selected.value === cropObject2.name){ 
-      resultadoFinal.innerHTML = `- The <span style= "color: Red;">cost</span> of buying <strong>${cropObject2.name}</strong> seeds will be <strong>${coste}</strong> star coins. <br>`
-      resultadoFinal.innerHTML += `- The <span style= "color: Green;">net profit</span> will be <strong>${beneficio}</strong> star coins <i>(Net profit = final profit - crop cost)</i>. <br>`
-      resultadoFinal.innerHTML += `- ${cropObject2.name} must be planted on <strong>"${cropObject2.location}"</strong>. <br>`
-      resultadoFinal.innerHTML += `- ${cropObject2.name} must be watered <strong>${cropObject2.waters} times</strong>. <br>`
-//convert hours to minutes when format is decimals on hours.
-      if (cropObject2.time < 1){
-        let timeMinutes = cropObject2.time * 60;
-        resultadoFinal.innerHTML += `- ${cropObject2.name} will take <strong>${timeMinutes} minutes </strong> to grow approximately (if watered when needed).`
-      } else {
-        resultadoFinal.innerHTML += `- ${cropObject2.name} will take <strong>${cropObject2.time} hours</strong> to grow approximately (if watered when needed).`
-      };
+      resultadoFinal.innerHTML = `📉The <span style= "color: Red; font-weight: bold;">cost</span> of buying <strong>${cropObject2.name}</strong> seeds will be <strong>${coste}</strong>      <img class="star" src="https://bit.ly/3rIu8mM" alt="star"/>
+      <br>`
+      resultadoFinal.innerHTML += `📈The <span style= "color: Green;  font-weight: bold;">net profit</span> will be <strong>${beneficio}</strong>      <img class="star" src="https://bit.ly/3rIu8mM" alt="star"/>
+      <i style="font-size: 12px;">(Net profit = final profit - crop cost)</i>. <br>`
+      resultadoFinal.innerHTML += `🛒<strong>${cropObject2.name} </strong>can be purchased on Goofy's <strong>"${cropObject2.location}"</strong> Stall. <br> `
+      resultadoFinal.innerHTML += `💧<strong>${cropObject2.name} </strong>must be <span style= "color: blue;  font-weight: bold;">watered</span> <strong>${cropObject2.waters} time/s</strong>. <br>`
+      resultadoFinal.innerHTML += `⏳<strong>${cropObject2.name}</strong> will take <strong>${cropObject2.time} </strong> to grow <i style="font-size: 12px;">(if watered when needed)</i>. <br>`
+      resultadoFinal.innerHTML += `✨If planted on <strong>"${cropObject2.location}"</strong>, it will take <strong>${cropObject2.bonusTime}</strong> to grow <i style="font-size: 12px;">(-10%).</i> <br>`
     };
   };
 };
